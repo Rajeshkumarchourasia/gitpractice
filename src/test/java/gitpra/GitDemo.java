1c8e3fe0a1ad686318b63e55071f0ba24d28f7c0
user <user@DESKTOP-QT6AUMT>
2022-04-30 11:00:25 +0530

    commit 1 git

---
+++ src/test/java/gitpra/GitDemo.java
@@ -0,0 +1,14 @@
+package gitpra;
+
+import org.testng.TestNG;
+import org.testng.annotations.Test;
+
+public class GitDemo {
+	
+	@Test
+	public void m1()
+	{
+		System.out.println("git 1 commit");
+	}
+
+}
